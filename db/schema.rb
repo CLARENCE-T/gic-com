0bcdf6b125276f8f1e3ce0209ce2d7aa0bc9462e
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-01-26 12:01:46 +0100

    about model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_104009) do
+ActiveRecord::Schema.define(version: 2021_01_26_110111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abouts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.text "description"

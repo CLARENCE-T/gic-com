4603829f777a3d6d6a8e6d5824aa70b679d01631
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-01-27 16:33:05 +0100

    bring some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
 p 'destroy everything'
 
@@ -14,5 +15,20 @@ Event.destroy_all
 About.destroy_all
 Picture.destroy_all
 
+p 'create user'
 
+geoffrey = User.create! email: 'gervais.geoffrey@gmail.com', password: '123456', password_confirmation: '123456', admin: true
 
+p 'create events'
+
+narcisse = Event.create! description: 'Narcisse Mag', date: Date.strptime('20/10/2017', '%d/%m/%Y')
+file = URI.open('https://res.cloudinary.com/dwklvsgae/image/upload/v1611752251/7z8xpuuzwtvrd85uf9af8lww7kic.jpg')
+narcisse.images.attach(io: file, filename: 'narcisse.png', content_type: 'image/jpg')
+
+off = Event.create! description: 'Soirée OFF', date: Date.strptime('23/09/2020', '%d/%m/%Y')
+file = URI.open('https://res.cloudinary.com/dwklvsgae/image/upload/v1611761114/EVENTS/OFF/OFF_Paris_Seine_c_Julien_Mouffron-Gardner-117_2_v5hx7o.jpg')
+off.images.attach(io: file, filename: 'narcisse.png', content_type: 'image/jpg')
+
+off = Event.create! description: 'Soirée OFF'
+file = URI.open('https://res.cloudinary.com/dwklvsgae/image/upload/v1611761177/EVENTS/LUTECIA%20JOSEPHINE/JustinPrinz-technogym-1195_rm2483.jpg')
+off.images.attach(io: file, filename: 'narcisse.png', content_type: 'image/jpg')

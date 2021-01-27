769ed937128bf30107af2e83519e364799114d2f
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-01-27 16:55:19 +0100

    debug

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,10 +25,10 @@ narcisse = Event.create! description: 'Narcisse Mag', date: Date.strptime('20/10
 file = URI.open('https://res.cloudinary.com/dwklvsgae/image/upload/v1611752251/7z8xpuuzwtvrd85uf9af8lww7kic.jpg')
 narcisse.images.attach(io: file, filename: 'narcisse.png', content_type: 'image/jpg')
 
-off = Event.create! description: 'Soirée OFF', date: Date.strptime('23/09/2020', '%d/%m/%Y')
-file = URI.open('https://res.cloudinary.com/dwklvsgae/image/upload/v1611761114/EVENTS/OFF/OFF_Paris_Seine_c_Julien_Mouffron-Gardner-117_2_v5hx7o.jpg')
-off.images.attach(io: file, filename: 'narcisse.png', content_type: 'image/jpg')
+# off = Event.create! description: 'Soirée OFF', date: Date.strptime('23/09/2020', '%d/%m/%Y')
+# file = URI.open('https://res.cloudinary.com/dwklvsgae/image/upload/v1611761114/EVENTS/OFF/OFF_Paris_Seine_c_Julien_Mouffron-Gardner-117_2_v5hx7o.jpg')
+# off.images.attach(io: file, filename: 'narcisse.png', content_type: 'image/jpg')
 
-lutecia = Event.create! description: 'Soirée OFF'
-file = URI.open('https://res.cloudinary.com/dwklvsgae/image/upload/v1611761177/EVENTS/LUTECIA%20JOSEPHINE/JustinPrinz-technogym-1195_rm2483.jpg')
-lutecia.images.attach(io: file, filename: 'narcisse.png', content_type: 'image/jpg')
+# lutecia = Event.create! description: 'Soirée OFF'
+# file = URI.open('https://res.cloudinary.com/dwklvsgae/image/upload/v1611761177/EVENTS/LUTECIA%20JOSEPHINE/JustinPrinz-technogym-1195_rm2483.jpg')
+# lutecia.images.attach(io: file, filename: 'narcisse.png', content_type: 'image/jpg')

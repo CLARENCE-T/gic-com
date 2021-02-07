88ada32865195bfcd11e6359778c4e161049b8d8
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-02-07 12:57:25 +0100

    finish cript

--- app/javascript/components/mosaic_zoom.js
+++ app/javascript/components/mosaic_zoom.js
@@ -14,31 +14,45 @@ const zoom = () => {
         var imgSelected = document.createElement("img");
         imgSelected.src = element1.src;
         imgSelected.alt = element1.alt;
+        imgSelected.id = element1.id;
         imgSelected.classList.add("fullscreen-pics");
         document.body.appendChild(imgSelected);
 
         //create arrow
         var next = document.createElement("i");
         next.classList.add("next", "fa", "fa-chevron-right");
+
         document.body.appendChild(next);
 
         var prev = document.createElement("i");
         prev.classList.add("prev", "fa", "fa-chevron-left");
         document.body.appendChild(prev);
 
+        //create cross
+        // var cross = document.createElement("i");
+        // cross.classList.add("cross", "fa", "fa-times");
+        // imgSelected.appendChild(cross);
+
         //click on next
-        next.addEventListener('click', (element2) => {
+        var numOfPics = document.querySelectorAll(".event-pics").length
 
-          console.log(imgSelected.id)
-
-
-
+        next.addEventListener('click', (next) => {
+          if ( parseInt(imgSelected.id) < numOfPics -1) {
+            var nextImageId = parseInt(imgSelected.id) + 1;
+            var nextImage = document.getElementById(nextImageId);
+            imgSelected.src = nextImage.src;
+            imgSelected.id = nextImage.id;
+          };
         });
 
         //click on prev
         prev.addEventListener('click', (element) => {
-          console.log('prev')
-
+          if ( 0 < parseInt(imgSelected.id)) {
+            var prevImageId = parseInt(imgSelected.id) - 1;
+            var prevImage = document.getElementById(prevImageId);
+            imgSelected.src = prevImage.src;
+            imgSelected.id = prevImage.id;
+          };
         });
 
 
@@ -51,7 +65,6 @@ const zoom = () => {
           next.remove()
           prev.remove()
           document.querySelector('.container').classList.toggle('blur')
-
         });
       };
 

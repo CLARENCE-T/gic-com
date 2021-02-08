ed66feccc47f6047e5432c484b0351f67ac56c85
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-02-08 15:55:54 +0100

    migrate and display

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_213147) do
+ActiveRecord::Schema.define(version: 2021_02_08_144213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_02_01_213147) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "presentation"
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|

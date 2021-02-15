43c5396732e9d52086ac5f3724543881c6f60d64
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-02-15 22:13:46 +0100

    end

---
+++ app/javascript/components/masonry-pictures.js
@@ -0,0 +1,21 @@
+import Masonry from 'masonry-layout';
+
+const masonry = () => {
+    if (document.querySelector('.masonry-grid-pictures')) {
+      new Masonry( '.masonry-grid-pictures', {
+        itemSelector: '.masonry-item-pictures',
+        columnWidth: '.grid-sizer',
+        percentPosition: true,
+        // gutter: 40,
+        // fitWidth: true,
+        // transitionDuration: 0,
+    });
+    console.log('masonry pictures done')
+  };
+};
+
+
+
+
+
+export { masonry }
--- app/javascript/components/smooth_scroll.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery'
-
-const smoothScroll = () => {
-
-}
-
-export {smoothScroll}

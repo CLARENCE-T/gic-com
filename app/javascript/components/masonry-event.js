4cd2e33e9ce8a244e7e53a5e98e8f9dbe824d5a7
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-02-16 20:47:44 +0100

    done events

--- app/javascript/components/masonry-event.js
+++ app/javascript/components/masonry-event.js
@@ -1,16 +1,16 @@
 import Masonry from 'masonry-layout';
 
 const masonryEvent = () => {
-  // if (document.querySelector('.masonry-grid-events')) {
-  //   new Masonry( '.masonry-grid-events', {
-  //     itemSelector: '.masonry-item-events',
-  //     percentPosition: true,
-  //     // percentPosition: true,
-  //     // fitWidth: true,
-  //     // gutter: 4,
-  //   });
-  //   console.log('masonry event  done')
-  // }
+  if (document.querySelector('.masonry-grid-events')) {
+    new Masonry( '.masonry-grid-events', {
+      itemSelector: '.masonry-item-events',
+      percentPosition: true,
+      // percentPosition: true,
+      // fitWidth: true,
+      // gutter: 4,
+    });
+    console.log('masonry event  done')
+  }
 };
 
 export { masonryEvent }

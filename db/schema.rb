849bf25185f394d6a2230bfa485c26be78a1b546
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-05-19 12:27:58 +0200

    migration fails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_080237) do
+ActiveRecord::Schema.define(version: 2021_05_19_094432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_04_01_080237) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "subtitle"
+    t.boolean "active"
   end
 
   create_table "logos", force: :cascade do |t|
